2cc1861cde85af76755172013e8a40b7ad8a11dd
Mira <szewczyk.mira@gmail.com>
2020-01-06 14:49:40 +0100

    Rename model field

--- exchangerate/rates/models.py
+++ exchangerate/rates/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Rate(models.Model):
-    currency_name = models.CharField(max_length=3, unique=True)
+    currency = models.CharField(max_length=3, unique=True)
     exchange_rate = models.DecimalField(decimal_places=4, max_digits=50)
     parser_update_date = models.DateTimeField()
     description = models.TextField()
